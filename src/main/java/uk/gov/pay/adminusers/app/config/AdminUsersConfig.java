b8c556712391d88081bdca859f87144ead3a471e
Kasun Kumara <kasun.kumara@gmail.com>
2017-02-16 14:50:39 +0000

    pp-1696 making container to check for new datasource at startup
    This change make adminusers service to lookout for adminusers database first. Failing which it will connect to selfservice database as usual. The expectation is that this will help us to seamlessly switch over to new database (without a code change) when we eventually migrate to new adminusers database by switching of selfservice database.
    
    We will have to remove this logic later on (after the migration)

--- src/main/java/uk/gov/pay/adminusers/app/config/AdminUsersConfig.java
+++ src/main/java/uk/gov/pay/adminusers/app/config/AdminUsersConfig.java
@@ -3,15 +3,27 @@ package uk.gov.pay.adminusers.app.config;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
+import org.slf4j.Logger;
+import uk.gov.pay.adminusers.logger.PayLoggerFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.sql.*;
 
-public class AdminUsersConfig extends Configuration{
+import static java.lang.String.format;
+
+
+public class AdminUsersConfig extends Configuration {
+
+    private static final Logger logger = PayLoggerFactory.getLogger(AdminUsersConfig.class);
 
     @Valid
     @NotNull
-    private DataSourceFactory dataSourceFactory;
+    private DataSourceFactory oldDataSourceFactory;
+
+    @Valid
+    @NotNull
+    private DataSourceFactory newDataSourceFactory;
 
     @Valid
     @NotNull
@@ -21,6 +33,7 @@ public class AdminUsersConfig extends Configuration{
     private String graphiteHost;
     @NotNull
     private String graphitePort;
+
     @NotNull
     private String baseUrl;
 
@@ -33,9 +46,26 @@ public class AdminUsersConfig extends Configuration{
     @NotNull
     private Integer timeStepsInSeconds;
 
-    @JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
-        return dataSourceFactory;
+        //temporary switch to check if adminusers database exists and can be used
+        if (shouldUseAdminUsersDatasource(getNewDataSourceFactory())) {
+            return getNewDataSourceFactory();
+        }
+        return getOldDataSourceFactory();
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getOldDataSourceFactory() {
+        return oldDataSourceFactory;
+    }
+
+    /**
+     * TODO: rename this annotation to `database` when we remove the above `getOldDataSourceFactory`
+     * @return
+     */
+    @JsonProperty("databaseNew")
+    public DataSourceFactory getNewDataSourceFactory() {
+        return newDataSourceFactory;
     }
 
     @JsonProperty("jpa")
@@ -66,5 +96,51 @@ public class AdminUsersConfig extends Configuration{
 
     public int getTimeStepsInSeconds() {
         return timeStepsInSeconds;
+    }
+
+    /**
+     * Temporary method which checks availability of adminusers database, or switch to selfservice if not available
+     * @param dataSourceFactory
+     * @return
+     */
+    private boolean shouldUseAdminUsersDatasource(DataSourceFactory dataSourceFactory) {
+        final String adminusersDb = "adminusers";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    dataSourceFactory.getUrl(),
+                    dataSourceFactory.getUser(),
+                    dataSourceFactory.getPassword());
+            connection.setReadOnly(true);
+            if (connection.isValid(2)) {
+                PreparedStatement statement = connection.prepareStatement("select exists (select * from pg_tables where tablename='users')");
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    boolean usersExists = resultSet.getBoolean(1);
+                    if (usersExists) {
+                        logger.info("datasource {} is available and USERS table exists. Assuming its ready to be used", adminusersDb);
+                        return true;
+                    } else {
+                        logger.info("datasource {} is available and USERS table does not exist. Switching to selfservice DB", adminusersDb);
+                    }
+                } else {
+                    logger.error("datasource {} is available. But pg_tables doesn't seem to exists. Check database setup", adminusersDb);
+                }
+            } else {
+                logger.warn("database {} is contactable. But connections cannot be made", adminusersDb);
+            }
+            return false;
+        } catch (Exception e) {
+            logger.info(format("datasource %s is not available > switching to selfservice database", adminusersDb), e);
+            return false;
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    logger.error("error closing test connection to " + adminusersDb, e);
+                }
+            }
+        }
     }
 }
