f98506e690ec1b779daa2354cea0b13f83e29f81
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2017-03-01 13:56:07 +0000

    PP-1598 Restrict gateway accounts in user creation.
        - The list of gateway accounts must match exactly the ones of a service.
        - If non of them are part of a service, a new service will be created.
        - A '409 Conflict' will be returned in case of any these conditions are not
        met.

--- src/test/java/uk/gov/pay/adminusers/resources/UserResourceCreateAndGetTest.java
+++ src/test/java/uk/gov/pay/adminusers/resources/UserResourceCreateAndGetTest.java
@@ -6,15 +6,13 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import uk.gov.pay.adminusers.model.User;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import static com.jayway.restassured.http.ContentType.JSON;
 import static java.lang.String.format;
-import static java.lang.String.valueOf;
 import static java.util.UUID.randomUUID;
-import static org.apache.commons.lang3.RandomStringUtils.*;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -25,10 +23,12 @@ public class UserResourceCreateAndGetTest extends UserResourceTestBase {
     public void shouldCreateAUserWithGatewayAccountIdFieldSuccessfully() throws Exception {
 
         String username = randomAlphanumeric(10) + randomUUID().toString();
+        String gatewayAccountId = randomAlphanumeric(5);
+
         ImmutableMap<Object, Object> userPayload = ImmutableMap.builder()
                 .put("username", username)
                 .put("email", "user-" + username + "@example.com")
-                .put("gateway_account_id", "1")
+                .put("gateway_account_id", gatewayAccountId)
                 .put("telephone_number", "45334534634")
                 .put("otp_key", "34f34")
                 .put("role_name", "admin")
@@ -46,9 +46,9 @@ public class UserResourceCreateAndGetTest extends UserResourceTestBase {
                 .body("username", is(username))
                 .body("password", nullValue())
                 .body("email", is("user-" + username + "@example.com"))
-                .body("gateway_account_id", is("1"))
+                .body("gateway_account_id", is(gatewayAccountId))
                 .body("gateway_account_ids", hasSize(1))
-                .body("gateway_account_ids[0]", is("1"))
+                .body("gateway_account_ids[0]", is(gatewayAccountId))
                 .body("telephone_number", is("45334534634"))
                 .body("otp_key", is("34f34"))
                 .body("login_counter", is(0))
@@ -61,15 +61,11 @@ public class UserResourceCreateAndGetTest extends UserResourceTestBase {
                 .body("role.description", is("Administrator"))
                 .body("permissions", hasSize(28)); //we could consider removing this assertion if the permissions constantly changing
 
-        //TODO - WIP PP-1483 This will be amended when the story is done.
+        //TODO - WIP This will be removed when PP-1612 is done.
         // This is an extra check to verify that new created user gateways are registered withing the new Services Model as well as in users table
         List<Map<String, Object>> userByName = databaseTestHelper.findUserByName(username);
         List<Map<String, Object>> servicesAssociatedToUser = databaseTestHelper.findUserServicesByUserId((Integer) userByName.get(0).get("id"));
         assertThat(servicesAssociatedToUser.size(), is(1));
-
-        List<Map<String, Object>> gatewayAccountsAssociatedToUser = databaseTestHelper.findGatewayAccountsByService((Long) servicesAssociatedToUser.get(0).get("service_id"));
-        assertThat(gatewayAccountsAssociatedToUser.size(), is(1));
-        assertThat(gatewayAccountsAssociatedToUser.get(0).get("gateway_account_id"), is("1"));
     }
 
     @Test
@@ -113,15 +109,11 @@ public class UserResourceCreateAndGetTest extends UserResourceTestBase {
                 .body("role.description", is("Administrator"))
                 .body("permissions", hasSize(28)); //we could consider removing this assertion if the permissions constantly changing
 
-        //TODO - WIP PP-1483 This will be amended when the story is done.
+        //TODO - WIP This will be removed when PP-1612 is done.
         // This is an extra check to verify that new created user gateways are registered withing the new Services Model as well as in users table
         List<Map<String, Object>> userByName = databaseTestHelper.findUserByName(username);
         List<Map<String, Object>> servicesAssociatedToUser = databaseTestHelper.findUserServicesByUserId((Integer) userByName.get(0).get("id"));
-        assertThat(servicesAssociatedToUser.size(), is(2));
-
-        List<Map<String, Object>> gatewayAccountsAssociatedToUser = databaseTestHelper.findGatewayAccountsByService((Long) servicesAssociatedToUser.get(0).get("service_id"));
-        assertThat(gatewayAccountsAssociatedToUser.size(), is(1));
-        assertThat(gatewayAccountsAssociatedToUser.get(0).get("gateway_account_id"), is("222"));
+        assertThat(servicesAssociatedToUser.size(), is(1));
     }
 
     @Test
@@ -166,15 +158,10 @@ public class UserResourceCreateAndGetTest extends UserResourceTestBase {
                 .body("role.description", is("Administrator"))
                 .body("permissions", hasSize(28)); //we could consider removing this assertion if the permissions constantly changing
 
-        //TODO - WIP PP-1483 This will be amended when the story is done.
-        // This is an extra check to verify that new created user gateways are registered withing the new Services Model as well as in users table
+        //TODO - WIP This will be removed when PP-1612 is done.
         List<Map<String, Object>> userByName = databaseTestHelper.findUserByName(username);
         List<Map<String, Object>> servicesAssociatedToUser = databaseTestHelper.findUserServicesByUserId((Integer) userByName.get(0).get("id"));
-        assertThat(servicesAssociatedToUser.size(), is(2));
-
-        List<Map<String, Object>> gatewayAccountsAssociatedToUser = databaseTestHelper.findGatewayAccountsByService((Long) servicesAssociatedToUser.get(0).get("service_id"));
-        assertThat(gatewayAccountsAssociatedToUser.size(), is(1));
-        assertThat(gatewayAccountsAssociatedToUser.get(0).get("gateway_account_id"), is("1"));
+        assertThat(servicesAssociatedToUser.size(), is(1));
     }
 
     @Test
