cabe41e9dc893794417016a66bde2bcb1f0b191a
Max Bladen-Clark <maxcbc@live.com>
2017-06-23 13:09:00 +0100

    PP-2167 2FA authentication against multiple timewindows - `SecondFactorAuthenticator.authorize` now checks against the current 30 time window and the previous four, leading to a total possible validity of 2 minutes and 30 seconds - Added tests to prove this.

--- src/main/java/uk/gov/pay/adminusers/service/SecondFactorAuthenticator.java
+++ src/main/java/uk/gov/pay/adminusers/service/SecondFactorAuthenticator.java
@@ -1,8 +1,12 @@
 package uk.gov.pay.adminusers.service;
 
+import com.google.inject.Inject;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
 import org.slf4j.Logger;
 import uk.gov.pay.adminusers.logger.PayLoggerFactory;
+
+import java.time.Clock;
 
 import static com.google.common.io.BaseEncoding.base32;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -11,21 +15,23 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 public class SecondFactorAuthenticator {
 
     private static final Logger logger = PayLoggerFactory.getLogger(SecondFactorAuthenticator.class);
-
     private final GoogleAuthenticator authenticator;
+    private final Clock clock;
 
-    public SecondFactorAuthenticator() {
-        this.authenticator = new GoogleAuthenticator();
+    @Inject
+    public SecondFactorAuthenticator(GoogleAuthenticatorConfig authenticatorConfig, Clock clock) {
+        this.clock = clock;
+        this.authenticator = new GoogleAuthenticator(authenticatorConfig);
     }
 
     public int newPassCode(String secret) {
         checkNull(secret);
-        return authenticator.getTotpPassword(base32().encode(secret.getBytes()));
+        return authenticator.getTotpPassword(base32().encode(secret.getBytes()), clock.millis());
     }
 
     public boolean authorize(String secret, int passcode) {
         checkNull(secret);
-        return authenticator.authorize(base32().encode(secret.getBytes()), passcode);
+        return authenticator.authorize(base32().encode(secret.getBytes()), passcode, clock.millis());
     }
 
     private void checkNull(String secret) {
