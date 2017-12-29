db1dc97b3f9019361da92d48709792c1ecebe5df
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2017-12-29 09:25:33 +0000

    pp-3096: fix chamber startup startup
    I broke this yesterday, this fixes the broken change and adds a new
    script which just calls the regular script but with chamber.
    
    solo @tlwr

--- docker-startup.sh
+++ docker-startup.sh
@@ -3,9 +3,4 @@
 java -jar *-allinone.jar waitOnDependencies *.yaml && \
 java -jar *-allinone.jar migrateToInitialDbState *.yaml && \
 java -jar *-allinone.jar db migrate *.yaml && \
-
-if [ -n "$CHAMBER" ]; then
-  AWS_REGION=${ECS_AWS_REGION} chamber exec ${ECS_SERVICE} -- java $JAVA_OPTS -jar *-allinone.jar server *.yaml
-else
-  java $JAVA_OPTS -jar *-allinone.jar server *.yaml
-fi
+java $JAVA_OPTS -jar *-allinone.jar server *.yaml
