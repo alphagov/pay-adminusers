bca8945dc19453a05b4a74308d0e7b17dfc2d9a1
Belinda Cowey <belinda.cowey@digital.cabinet-office.gov.uk>
2018-01-22 10:08:14 +0000

    PP-3174 Implement database migrations for ECS
    Remove docker-startup-with-db-migration and replace
    with docker-startup with logic

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-java -jar *-allinone.jar waitOnDependencies *.yaml && \
-java $JAVA_OPTS -jar *-allinone.jar server *.yaml
+set -eu
+RUN_MIGRATION=${RUN_MIGRATION:-false}
+RUN_APP=${RUN_APP:-true}
+
+java -jar *-allinone.jar waitOnDependencies *.yaml
+
+if [ "$RUN_MIGRATION" == "true" ]; then
+  java -jar *-allinone.jar migrateToInitialDbState *.yaml
+  java -jar *-allinone.jar db migrate *.yaml
+fi
+
+if [ "$RUN_APP" == "true" ]; then
+  java -jar *-allinone.jar server *.yaml
+fi
+
+exit 0
