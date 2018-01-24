6cce4f4ca02daa55220776f2f0985b7037a6d411
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-01-24 16:56:18 +0000

    BAU: Use chamber in base image
    chamber is now in the base image for java and node
    
    we don't vendor it anymore
    
    and it is in /usr/bin so it is in the path
    
    solo @tlwr

--- run-with-chamber.sh
+++ run-with-chamber.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-AWS_REGION="${ECS_AWS_REGION}" ./chamber--linux-amd64 exec "${ECS_SERVICE}" -- ./docker-startup.sh
+AWS_REGION="${ECS_AWS_REGION}" chamber exec "${ECS_SERVICE}" -- ./docker-startup.sh
