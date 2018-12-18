5e5f84bffecb96b159595c98bb5e4e707a8be490
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2018-12-18 07:15:14 +0000

    BAU: Remove no-op overridden method from DependentResourceWaitCommand
    The 'configure' method just calls the superclass. Java can do this for us.
    Remove the method.

--- src/main/java/uk/gov/pay/adminusers/app/healthchecks/DependentResourceWaitCommand.java
+++ src/main/java/uk/gov/pay/adminusers/app/healthchecks/DependentResourceWaitCommand.java
@@ -3,17 +3,11 @@ package uk.gov.pay.adminusers.app.healthchecks;
 import io.dropwizard.cli.ConfiguredCommand;
 import io.dropwizard.setup.Bootstrap;
 import net.sourceforge.argparse4j.inf.Namespace;
-import net.sourceforge.argparse4j.inf.Subparser;
 import uk.gov.pay.adminusers.app.config.AdminUsersConfig;
 
 public class DependentResourceWaitCommand extends ConfiguredCommand<AdminUsersConfig> {
     public DependentResourceWaitCommand() {
         super("waitOnDependencies", "Waits for dependent resources to become available");
-    }
-
-    @Override
-    public void configure(Subparser subparser) {
-        super.configure(subparser);
     }
 
     @Override
