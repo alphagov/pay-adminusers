69a6b30614c5b30af9173d0e4faa19cac469d8da
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-02-05 09:46:15 +0000

    Simplify GovUkPayAgreementRequestValidator.validateCreateRequest
    Trick out this method with a bit of functional style

--- src/main/java/uk/gov/pay/adminusers/resources/GovUkPayAgreementRequestValidator.java
+++ src/main/java/uk/gov/pay/adminusers/resources/GovUkPayAgreementRequestValidator.java
@@ -5,7 +5,6 @@ import com.google.inject.Inject;
 import uk.gov.pay.adminusers.utils.Errors;
 import uk.gov.pay.adminusers.validations.RequestValidations;
 
-import java.util.List;
 import java.util.Optional;
 
 public class GovUkPayAgreementRequestValidator {
@@ -19,14 +18,9 @@ public class GovUkPayAgreementRequestValidator {
     }
     
     public Optional<Errors> validateCreateRequest(JsonNode payload) {
-        Optional<List<String>> missingMandatoryFields = requestValidations.checkExistsAndNotEmpty(payload, FIELD_USER_ID);
-        if (missingMandatoryFields.isPresent()) {
-            return Optional.of(Errors.from(missingMandatoryFields.get()));
-        }
-        Optional<List<String>> invalidType = requestValidations.checkIsString("Field [%s] must be a valid user ID", payload, FIELD_USER_ID);
-        if (invalidType.isPresent()) {
-            return Optional.of(Errors.from(invalidType.get()));
-        }
-        return Optional.empty();
+        return requestValidations.checkExistsAndNotEmpty(payload, FIELD_USER_ID)
+                .map(strings -> Optional.of(Errors.from(strings)))
+                .orElseGet(() -> requestValidations.checkIsString("Field [%s] must be a valid user ID", payload, FIELD_USER_ID)
+                .map(Errors::from));
     }
 }
