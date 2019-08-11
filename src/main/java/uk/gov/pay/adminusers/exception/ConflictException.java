3abd585c6bfdcdfcea71780538428da3bf7eb77d
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-08-11 20:27:07 +0100

    Make a couple of exceptions package-private
    These exceptions are only ever subclassed or referenced by their respective
    ExceptionMappers in the same package, so make them package-private.

--- src/main/java/uk/gov/pay/adminusers/exception/ConflictException.java
+++ src/main/java/uk/gov/pay/adminusers/exception/ConflictException.java
@@ -1,8 +1,7 @@
 package uk.gov.pay.adminusers.exception;
 
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
+class ConflictException extends RuntimeException {
+    ConflictException(String message) {
         super(message);
     }
 }
