776f45cf562102ce995ab40326b8400ba0ec8772
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-02 14:01:48 +0100

    PP-5670: Specify the consumer when running contract test
    Currently when a consumer build is kicked off, the provider contract test will
    run in its entirety. For example, in a selfservice build, the adminusers provider
    contract tests will run as well. However, what is run is:
    
    products-ui->adminusers
    selfservice->adminusers
    
    We really only want to run selfservice->adminusers in this case.
    
    This commit will allow a dynamic creation of the test suite to run based on a
    CONSUMER system property. This will mean shorter run times when a provider
    contract test is run as part of a specific consumer build.

---
+++ src/test/java/uk/gov/pay/adminusers/pact/ProductsUIContractTest.java
@@ -0,0 +1,15 @@
+package uk.gov.pay.adminusers.pact;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
+import org.junit.runner.RunWith;
+
+@RunWith(PactRunner.class)
+@Provider("adminusers")
+@PactBroker(scheme = "https", host = "pact-broker-test.cloudapps.digital", tags = {"${PACT_CONSUMER_TAG}", "test"},
+        authentication = @PactBrokerAuth(username = "${PACT_BROKER_USERNAME}", password = "${PACT_BROKER_PASSWORD}")
+        , consumers = {"products-ui"})
+public class ProductsUIContractTest extends ContractTest {
+}
