02f0936ae8dac1022361a3d8969c344aa0c2c03c
Alex Bishop <alexbishop1@users.noreply.github.com>
2019-10-04 14:19:30 +0100

    Introduce ShellCheck
    Fix or ignore errors in shell scripts found by ShellCheck.
    
    ShellCheck reads a shellcheckrc file in the project directory,
    which can contain directives like disable=SC2059 to ignore certain
    errors.
    
    Add a shellcheckrc file with no directives (so far, everything
    we’ve wanted to ignore has been best dealt with on a case-by-case
    basis) for the benefit of Codacy.

--- docker-startup.sh
+++ docker-startup.sh
@@ -5,13 +5,17 @@ set -eu
 : "${RUN_MIGRATION:=false}"
 : "${RUN_APP:=true}"
 
-java $JAVA_OPTS -jar *-allinone.jar waitOnDependencies *.yaml
+# shellcheck disable=SC2086
+java $JAVA_OPTS -jar ./*-allinone.jar waitOnDependencies ./*.yaml
 
 if [ "$RUN_MIGRATION" == "true" ]; then
-  java $JAVA_OPTS -jar *-allinone.jar migrateToInitialDbState *.yaml
-  java $JAVA_OPTS -jar *-allinone.jar db migrate *.yaml
+  # shellcheck disable=SC2086
+  java $JAVA_OPTS -jar ./*-allinone.jar migrateToInitialDbState ./*.yaml
+  # shellcheck disable=SC2086
+  java $JAVA_OPTS -jar ./*-allinone.jar db migrate ./*.yaml
 fi
 
 if [ "$RUN_APP" == "true" ]; then
-  exec java $JAVA_OPTS -jar *-allinone.jar server *.yaml
+  # shellcheck disable=SC2086
+  exec java $JAVA_OPTS -jar ./*-allinone.jar server ./*.yaml
 fi
