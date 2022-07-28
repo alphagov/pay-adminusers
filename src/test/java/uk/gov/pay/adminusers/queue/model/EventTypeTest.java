1a6134f222c2328cb3ad9515b20fd2edb7d0ce33
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2022-07-28 11:39:03 +0100

    PP-9671 Handle unknown event types
    - Handle unknown event types better. The current way of getting EventType enum throws an exception if an event type is not in enum constants

---
+++ src/test/java/uk/gov/pay/adminusers/queue/model/EventTypeTest.java
@@ -0,0 +1,30 @@
+package uk.gov.pay.adminusers.queue.model;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static uk.gov.pay.adminusers.queue.model.EventType.DISPUTE_CREATED;
+import static uk.gov.pay.adminusers.queue.model.EventType.UNKNOWN;
+
+class EventTypeTest {
+
+    @Test
+    void shouldGetCorrectEventTypeForValidValue() {
+        assertThat(EventType.byType("DISPUTE_CREATED"), is(DISPUTE_CREATED));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "",
+            "some-random-string"
+    })
+    @NullSource
+    void shouldReturnUnknownForEmptyValue(String value) {
+        assertThat(EventType.byType(value), is(UNKNOWN));
+    }
+}
