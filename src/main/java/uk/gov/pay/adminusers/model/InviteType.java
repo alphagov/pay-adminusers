7582e09a5ce4788e8aa29de95e77d308df9e7a78
Alan <alan.carter@digital.cabinet-office.gov.uk>
2022-10-06 11:14:47 +0100

    PP-10041: Make InviteType parsing more robust

--- src/main/java/uk/gov/pay/adminusers/model/InviteType.java
+++ src/main/java/uk/gov/pay/adminusers/model/InviteType.java
@@ -1,35 +1,21 @@
 package uk.gov.pay.adminusers.model;
 
-import static java.lang.String.format;
-
 public enum InviteType {
 
     // Old values; these are still in use.
-    USER("user"),
-    SERVICE("service"),
+    USER,
+    SERVICE,
 
     // New values; these are not used yet.
-    EXISTING_USER_INVITED_TO_EXISTING_SERVICE("existing_user_invited_to_existing_service"),
-    NEW_USER_INVITED_TO_EXISTING_SERVICE("new_user_invited_to_existing_service"),
-    NEW_USER_AND_NEW_SERVICE_SELF_SIGNUP("new_user_new_service_self_signup");
-
-    private String type;
-
-    InviteType(String type) {
-        this.type = type;
-    }
+    EXISTING_USER_INVITED_TO_EXISTING_SERVICE,
+    NEW_USER_INVITED_TO_EXISTING_SERVICE,
+    NEW_USER_AND_NEW_SERVICE_SELF_SIGNUP;
 
     public String getType() {
-        return type;
+        return name().toLowerCase();
     }
 
-    public static InviteType from(String inviteType) {
-        if (USER.type.equals(inviteType)) {
-            return USER;
-        } else if (SERVICE.type.equals(inviteType)) {
-            return SERVICE;
-        } else {
-            throw new RuntimeException(format("invalid invite type: [%s]", inviteType));
-        }
+    public static InviteType from(String typeString) {
+        return InviteType.valueOf(typeString.toUpperCase());
     }
 }
