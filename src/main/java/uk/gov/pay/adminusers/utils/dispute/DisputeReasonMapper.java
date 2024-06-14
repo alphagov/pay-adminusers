e9eb10d5f38f79dd254a3ec3c6b2b39989257788
Alex Bishop <alexbishop1@users.noreply.github.com>
2024-06-14 12:01:40 +0100

    BAU — Replace switch statement with switch expression with patterns
    Replace a switch statement with a switch expression (with pattern
    matching!) just because we can now we’re using Java 21.
    
    Add some more tests (which pass with both the old code and the new
    code to ensure we’re not changing any behaviour).
    
    (The old code used Apache Commons’ StringUtils.isBlank(…), which
    uses Character.isWhitespace(char), which does not support
    supplementary characters. The new code uses String.isBlank(…),
    which uses Character.isWhitespace(int), which does support
    supplementary characters. It is my understanding that all
    whitespace characters belong to the basic multilingual plane, so
    this should not change any behaviour. If I am wrong, the behaviour
    change is likely to be inconsequential: some "other" outputs may
    become "unknown" — but it seems extremely unlikely Stripe would
    send us reasons containing less common whitespace characters in the
    first place.)

--- src/main/java/uk/gov/pay/adminusers/utils/dispute/DisputeReasonMapper.java
+++ src/main/java/uk/gov/pay/adminusers/utils/dispute/DisputeReasonMapper.java
@@ -1,30 +1,18 @@
 package uk.gov.pay.adminusers.utils.dispute;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
 public class DisputeReasonMapper {
 
     public static String mapToNotifyEmail(String stripeReason) {
-        if (isBlank(stripeReason)) {
-            return "unknown";
-        }
-        switch (stripeReason) {
-            case "duplicate":
-            case "fraudulent":
-            case "general":
-                return stripeReason;
-            case "credit_not_processed":
-                return "credit not processed";
-            case "product_not_received":
-                return "product not received";
-            case "product_unacceptable":
-                return "product unacceptable";
-            case "subscription_canceled":
-                return "subscription cancelled";
-            case "unrecognized":
-                return "unrecognised";
-            default:
-                return "other";
-        }
+        return switch (stripeReason) {
+            case null -> "unknown";
+            case String reason when reason.isBlank() -> "unknown";
+            case "duplicate", "fraudulent", "general" -> stripeReason;
+            case "credit_not_processed" -> "credit not processed";
+            case "product_not_received" -> "product not received";
+            case "product_unacceptable" -> "product unacceptable";
+            case "subscription_canceled" -> "subscription cancelled";
+            case "unrecognized" -> "unrecognised";
+            default -> "other";
+        };
     }
 }
