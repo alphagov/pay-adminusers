9ac402eb004eaa46476cc9b2ee6de5fe85d03993
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-09-09 09:39:40 +0100

    use nicer collect style

--- src/main/java/uk/gov/pay/adminusers/model/ServiceNamesDeserializer.java
+++ src/main/java/uk/gov/pay/adminusers/model/ServiceNamesDeserializer.java
@@ -7,17 +7,15 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import uk.gov.service.payments.commons.model.SupportedLanguage;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toUnmodifiableMap;
 
 public class ServiceNamesDeserializer extends JsonDeserializer<Map<SupportedLanguage, String>> {
     @Override
     public Map<SupportedLanguage, String> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        Map<SupportedLanguage, String> supportedLanguageToServiceName = new HashMap<>();
-        jsonParser.getCodec().readValue(jsonParser, new TypeReference<Map<String, String>>() {})
-                .forEach((key, value) -> 
-                        supportedLanguageToServiceName.put(SupportedLanguage.fromIso639AlphaTwoCode(key), value));
-        return Collections.unmodifiableMap(supportedLanguageToServiceName);
+        return jsonParser.getCodec().readValue(jsonParser, new TypeReference<Map<String, String>>() {})
+                .entrySet().stream()
+                .collect(toUnmodifiableMap(entry -> SupportedLanguage.fromIso639AlphaTwoCode(entry.getKey()), Map.Entry::getValue));
     }
 }
