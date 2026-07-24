32c905f5174a4239a218f7c1c81ecb3949d283c0
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2026-07-24 12:02:17 +0100

    PP-15598 refactor if to switch

--- src/test/java/uk/gov/pay/adminusers/resources/ServiceUpdateOperationValidatorTest.java
+++ src/test/java/uk/gov/pay/adminusers/resources/ServiceUpdateOperationValidatorTest.java
@@ -18,7 +18,7 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class ServiceUpdateOperationValidatorTest {
+class ServiceUpdateOperationValidatorTest {
 
     private static final String GO_LIVE_STAGE_INVALID_ERROR_MESSAGE = "Field [value] must be one of [NOT_STARTED, ENTERED_ORGANISATION_NAME, ENTERED_ORGANISATION_ADDRESS, CHOSEN_PSP_ADYEN, CHOSEN_PSP_STRIPE, CHOSEN_PSP_GOV_BANKING_WORLDPAY, GOV_BANKING_MOTO_OPTION_COMPLETED, TERMS_AGREED_ADYEN, TERMS_AGREED_STRIPE, TERMS_AGREED_GOV_BANKING_WORLDPAY, DENIED, LIVE]";
     private static final String PSP_TEST_ACCOUNT_STAGE_INVALID_ERROR_MESSAGE = "Field [value] must be one of [NOT_STARTED, REQUEST_SUBMITTED, CREATED]";
@@ -27,7 +27,7 @@ public class ServiceUpdateOperationValidatorTest {
     private final ServiceUpdateOperationValidator serviceUpdateOperationValidator = new ServiceUpdateOperationValidator(new RequestValidations());
 
     @Test
-    public void shouldFail_whenUpdate_whenMissingRequiredField() {
+    void shouldFail_whenUpdate_whenMissingRequiredField() {
         Map<String, String> payload = Map.of("value", "example-name");
 
         List<String> errors = serviceUpdateOperationValidator.validate(mapper.valueToTree(payload));
@@ -38,12 +38,12 @@ public class ServiceUpdateOperationValidatorTest {
     }
 
     @Test
-    public void shouldFail_whenUpdate_whenInvalidPath() {
+    void shouldFail_whenUpdate_whenInvalidPath() {
         shouldFail("xyz", "replace", "any value", "Path [xyz] is invalid");
     }
 
     @Test
-    public void shouldFail_whenUpdateServiceName_whenPathContainsUnsupportedLanguage() {
+    void shouldFail_whenUpdateServiceName_whenPathContainsUnsupportedLanguage() {
         shouldFail("service_name/xx", "replace", "example name", "Path [service_name/xx] is invalid");
     }
 
@@ -78,7 +78,7 @@ public class ServiceUpdateOperationValidatorTest {
 
     @ParameterizedTest
     @MethodSource("shouldFailForOperationParams")
-    public void shouldFailForOperation(String operation, String path, Object value) {
+    void shouldFailForOperation(String operation, String path, Object value) {
         String expectedErrorMessage = String.format("Operation [%s] is invalid for path [%s]", operation, path);
         shouldFail(path, operation, value, expectedErrorMessage);
     }
@@ -115,7 +115,7 @@ public class ServiceUpdateOperationValidatorTest {
 
     @ParameterizedTest
     @MethodSource("replaceShouldFailWhenValueInvalidValueParameters")
-    public void replaceShouldFailWhenInvalidValue(String path, Object value, String expectedErrorMessage) {
+    void replaceShouldFailWhenInvalidValue(String path, Object value, String expectedErrorMessage) {
         shouldFail(path, "replace", value, expectedErrorMessage);
     }
 
@@ -141,7 +141,7 @@ public class ServiceUpdateOperationValidatorTest {
             "archived",
             "went_live_date"
     })
-    public void replaceShouldFailWhenValueMissing(String path) {
+    void replaceShouldFailWhenValueMissing(String path) {
         ObjectNode payload = mapper.createObjectNode();
         payload.put("path", path);
         payload.put("op", "replace");
@@ -172,7 +172,7 @@ public class ServiceUpdateOperationValidatorTest {
             "archived",
             "went_live_date"
     })
-    public void replaceShouldFailWhenValueIsNull(String path) {
+    void replaceShouldFailWhenValueIsNull(String path) {
         ObjectNode payload = mapper.createObjectNode();
         payload.put("path", path);
         payload.put("op", "replace");
@@ -193,7 +193,7 @@ public class ServiceUpdateOperationValidatorTest {
             "merchant_details/address_country",
             "merchant_details/address_postcode",
     })
-    public void replaceShouldFailWhenValueIsEmptyString(String path) {
+    void replaceShouldFailWhenValueIsEmptyString(String path) {
         shouldFail(path, "replace", "", "Field [value] is required");
     }
 
@@ -214,12 +214,12 @@ public class ServiceUpdateOperationValidatorTest {
 
     @ParameterizedTest
     @MethodSource("shouldFailWhenStringValueIsTooLongParameters")
-    public void replaceShouldFailWhenStringValueIsTooLong(String path, int expectedMaxLength) {
+    void replaceShouldFailWhenStringValueIsTooLong(String path, int expectedMaxLength) {
         shouldFail(path, "replace", randomAlphanumeric(expectedMaxLength + 1), String.format("Field [value] must have a maximum length of %s characters", expectedMaxLength));
     }
     
-    @Test 
-    public void addShouldFailWhenFeatureIsInvalid(){
+    @Test
+    void addShouldFailWhenFeatureIsInvalid(){
         shouldFail("feature", "add", "invalidFeature", "Field [value] must be one of [test_feature]");
     }
 
@@ -259,7 +259,7 @@ public class ServiceUpdateOperationValidatorTest {
 
     @ParameterizedTest
     @MethodSource("shouldSucceedParams")
-    public void shouldSucceed(String operation, String path, Object value) {
+    void shouldSucceed(String operation, String path, Object value) {
         Map<String, Object> payload = new HashMap<>();
         payload.put("path", path);
         payload.put("op", operation);
